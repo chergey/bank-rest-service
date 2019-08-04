ca293a03bc0760894e8107e88edf8bc03f2f3cbc
Chaulin Cergey <cossack5@mail.ru>
2019-08-04 07:17:42 +0300

    refactor code

--- app/src/main/java/org/elcer/accounts/app/TestUtils.java
+++ app/src/main/java/org/elcer/accounts/app/TestUtils.java
@@ -1,26 +1,22 @@
 package org.elcer.accounts.app;
 
 
+import io.vavr.Value;
 import io.vavr.control.Try;
 import lombok.experimental.UtilityClass;
 
 import java.util.List;
 
 @UtilityClass
+@SuppressWarnings("deprecation")
 public class TestUtils {
 
     public static final boolean TEST = isTest();
 
     private static boolean isTest() {
-       return Try.sequence(List.of( Try.of(() -> {
-                   Class.forName("org.junit.Test");
-                   return true;
-               }),
-               Try.of(() -> {
-                   Class.forName("org.junit.jupiter.api.Test");
-                   return true;
-               })
-       )).flatMap(f-> f.toTry()).getOrElse(false);
+        return Try.sequence(List.of(Try.run(() -> Class.forName("org.junit.Test")),
+                Try.run(() -> Class.forName("org.junit.jupiter.api.Test"))
+        )).flatMap(Value::toTry).map(f -> true).getOrElse(false);
 
     }
 
